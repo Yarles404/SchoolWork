9a696bbc27eb30120699152c7062d171f7d0e8da
Yarles404 <yarlescy@gmail.com>
2022-01-18 10:24:10 -0600

    stat477 added

---
+++ stat477/hw1/hw1.R
@@ -0,0 +1,14 @@
+##Homework Assignment 1 Template
+
+library(plyr) #count() function
+library(ggplot2) #ggplot() function
+
+##Problem 2
+##Read in the data file
+  ClassSpring22<- read.csv(file.choose(), header = T)
+
+  ##Variable 1
+
+  ##Variable 2
+
+ 
